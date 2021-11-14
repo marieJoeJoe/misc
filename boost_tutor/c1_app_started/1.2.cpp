38e130319b8ec0f464a14d2c737abb26f0a99f41
marie <marieqoeqoe@gmail.com>
2021-11-14 17:53:23 +0800

    1.1-3

---
+++ boost_tutor/c1_app_started/1.2.cpp
@@ -0,0 +1,26 @@
+#include <boost/any.hpp>
+#include <iostream>
+#include <vector>
+#include <string>
+
+int main(int argc, char* argv[])
+{
+
+  std::vector<boost::any> some_values;
+
+  some_values.push_back(10);
+
+  const char* c_str = "Hello there!";
+
+  some_values.push_back(c_str);
+
+  some_values.push_back(std::string("Wow!"));
+
+  std::string& s = boost::any_cast<std::string&>(some_values.back());
+
+  s += " This is great!\n";
+  
+  std::cout << s;
+
+  return 0;
+}
---
+++ boost_tutor/c1_app_started/1.3.cpp
@@ -0,0 +1,12 @@
+#include <boost/any.hpp>
+#include <iostream>
+#include <vector>
+#include <string>
+
+int main(int argc, char* argv[])
+{
+
+  std::vector<boost::any> some_values;
+
+  return 0;
+}
