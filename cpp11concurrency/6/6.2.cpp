c62e2aea899658647f7fda7aef9161e26e5ac92e
marie <marieqoeqoe@gmail.com>
2022-01-09 00:52:45 +0800

    6.2.cpp

--- cpp11concurrency/6/6.2.cpp
+++ cpp11concurrency/6/6.2.cpp
@@ -11,17 +11,17 @@ std::list<std::string> r_split(const std::string& str, const std::regex& delim) 
     const std::sregex_token_iterator begin { str.cbegin(), str.cend(), delim, -1 }, end;
     return { begin, end };
 }
-/*
-std::string params_cut="params/mh100/MH100-F7D34A97/cut";
-std::string params_area_remove="params/mh100/MH100-F7D34A97/area/remove";
-std::string params_area_list="params/mh100/MH100-F7D34A97/area/list";
-std::string params_speed="params/mh100/MH100-F7D34A97/speed";
-std::string control_device="control/mh100/MH100-F7D34A97/device";
-std::string params_area_add="params/mh100/MH100-F7D34A97/area/add";
-std::string update_check_progress="update/mh100/MH100-F7D34A97/progress";
-std::string update_check_check="update/mh100/MH100-F7D34A97/check";
-std::string status ="/status/mh100/MH100-F7D34A97/device";
-*/
+
+std::string m_params_cut="params/mh100/MH100-F7D34A97/cut";
+std::string m_params_area_remove="params/mh100/MH100-F7D34A97/area/remove";
+std::string m_params_area_list="params/mh100/MH100-F7D34A97/area/list";
+std::string m_params_speed="params/mh100/MH100-F7D34A97/speed";
+std::string m_control_device="control/mh100/MH100-F7D34A97/device";
+std::string m_params_area_add="params/mh100/MH100-F7D34A97/area/add";
+std::string m_update_check_progress="update/mh100/MH100-F7D34A97/progress";
+std::string m_update_check_check="update/mh100/MH100-F7D34A97/check";
+std::string m_status ="/status/mh100/MH100-F7D34A97/device";
+std::string m_status_e ="/status/mh100/MH100-F7D34A97/";
 
 std::string params_cut="params/mh100/cut";
 std::string params_area_remove="params/mh100/area/remove";
@@ -45,6 +45,22 @@ std::vector<std::string> ss = {
   update_check_check,
   status,
 };
+
+
+std::vector<std::string> m_ss = {
+  m_params_cut,
+  m_params_area_remove,
+  m_params_area_list,
+  m_params_speed,
+  m_control_device,
+  m_params_area_add,
+  m_update_check_progress,
+  m_update_check_check,
+  m_status,
+  m_status_e,
+};
+
+
 
 void get_device_sn(std::string& sn){
     sn.clear();
@@ -101,13 +117,18 @@ void mqtt_to_mcu_topic(std::string& mqtt_topic){
   std::string device_sn;
   get_device_sn(device_sn);
   device_sn += "/";
+  auto iter1 = mqtt_topic.begin();
+  auto iter0 = mqtt_topic.begin();
   auto iter = mqtt_topic.begin();
   for (; slash_count; iter++)
   {
-        if ('/' == *iter) slash_count--;
-        //iter++;
+        if ('/' == *iter){
+            slash_count--;
+            if(1 == slash_count) iter1 = iter;
+            if(0 == slash_count) iter0 = iter;
+        }
   }
-  mqtt_topic.erase(iter,device_sn.begin(),device_sn.end());
+  mqtt_topic.erase(iter1+1,iter0+1);
 }
 
 int main (int argc, char* argv[]) {
@@ -126,15 +147,27 @@ int main (int argc, char* argv[]) {
   }
   std::cout << s << std::endl;
 */
-  std::vector<std::string> result=split(s, delimiter);
+  //std::vector<std::string> result=split(s, delimiter);
 
-  std::cout<<result<<std::endl;
+  //std::cout<<result<<std::endl;
+
+  std::cout << "mcu_to_mqtt:"<<'\n';
   
   for(auto e:ss){
       std::cout << e<<'\n';
       mcu_to_mqtt_topic(e);
       std::cout << e<<'\n';
   }
+
+
+  std::cout << "mqtt_to_mcu:"<<'\n';
+
+  for(auto e:m_ss){
+      std::cout << e<<'\n';
+      mqtt_to_mcu_topic(e);
+      std::cout << e<<'\n';
+  }
+
   return 0;
 }
 
